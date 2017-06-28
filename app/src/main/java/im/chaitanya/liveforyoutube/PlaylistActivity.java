5f0bc11ff5daf578b6fc2b2b7e4deea7579801b4
Sudhir Khanger <sudhir@sudhirkhanger.com>
2017-06-28 15:52:44 +0530

    feat: implement basic YouTubeView
    This commit adds YouTubeView, updated google-services.json, and string
    resources for error.

--- app/src/main/java/im/chaitanya/liveforyoutube/PlaylistActivity.java
+++ app/src/main/java/im/chaitanya/liveforyoutube/PlaylistActivity.java
@@ -1,10 +1,13 @@
 package im.chaitanya.liveforyoutube;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +24,10 @@ public class PlaylistActivity extends YouTubeBaseActivity implements YouTubePlay
     FirebaseUser firebaseUser;
     String roomID;
 
+    private static final int RECOVERY_REQUEST = 1;
+    public static final String YT_DEV_KEY = "AIzaSyACWSrV0r6-HoRBr4f1XyTl1qynRa8fRkc";
+    private YouTubePlayerView youTubeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +39,38 @@ public class PlaylistActivity extends YouTubeBaseActivity implements YouTubePlay
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
         databaseReference.child(roomID).child("users").child(firebaseUser.getUid()).onDisconnect().setValue(null);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(YT_DEV_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            youTubePlayer.loadPlaylist(roomID);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), youTubeInitializationResult.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(YT_DEV_KEY, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
